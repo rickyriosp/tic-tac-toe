f5c0ea39f3e0271d1b6dd297a42ac8e4ebbf8340
rikiriosp <riki.rios91@gmail.com>
2021-01-21 00:06:44 -0500

    completed stage 5

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -6,29 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter cells: ");
-        String cells = scanner.nextLine();
-
         int x = 0;
         int o = 0;
         int rowX = 0;
         int rowO = 0;
         boolean space = false;
-        char[][] cellsArr  = new char[3][3];
+        boolean playerX = true;
+        char[][] cells  = new char[][] {{'_','_','_'},{'_','_','_'}, {'_','_','_'}};
 
-        for (int i = 0; i < 9; i++) {
-            char cell = cells.charAt(i);
-            cellsArr[i / 3][i - (i / 3) * 3] = cell;
-            if (cell == 'X') {
-                x++;
-            } else if (cell == 'O') {
-                o++;
-            } else {
-                space = true;
-            }
-        }
-
-        printBoard(cellsArr);
+        printBoard(cells);
 
         int num1;
         int num2;
@@ -53,53 +39,74 @@ public class Main {
                 num2--;
             }
 
-            if (cellsArr[num1][num2] != '_') {
+            if (cells[num1][num2] != '_') {
                 System.out.println("This cell is occupied! Choose another one!");
+                continue;
             } else {
-                cellsArr[num1][num2] = 'X';
-                printBoard(cellsArr);
+                if (playerX) {
+                    cells[num1][num2] = 'X';
+                    playerX = false;
+                    x++;
+                } else {
+                    cells[num1][num2] = 'O';
+                    playerX = true;
+                    o++;
+                }
+                printBoard(cells);
+
+                // Check game status
+                space = false;
+                for (int i = 0; i < 3; i++) {
+                    if (cells[i][0] == cells[i][1] && cells[i][0] == cells[i][2]) {
+                        if (cells[i][0] == 'X') {
+                            rowX++;
+                        } else if (cells[i][0] == 'O') {
+                            rowO++;
+                        }
+                    }
+                    if (cells[0][i] == cells[1][i] && cells[0][i] == cells[2][i]) {
+                        if (cells[0][i] == 'X') {
+                            rowX++;
+                        } else if (cells[0][i] == 'O') {
+                            rowO++;
+                        }
+                    }
+
+                    for (int j = 0; j < 3; j++) {
+                        if (cells[i][j] == '_') {
+                            space = true;
+                            break;
+                        }
+                    }
+                }
+                if ((cells[0][0] == cells[1][1] && cells[0][0] == cells[2][2]) ||
+                        (cells[0][2] == cells[1][1] && cells[0][2] == cells[2][0])) {
+                    if (cells[1][1] == 'X') {
+                        rowX++;
+                    } else if (cells[1][1] == 'O') {
+                        rowO++;
+                    }
+                }
+            }
+
+            // Output result
+            if (x - o > 1 || o - x > 1 || (rowX > 0 && rowO > 0)) {
+                System.out.println("Impossible");
                 break;
+            } else if (rowX > 0) {
+                System.out.println("X wins");
+                break;
+            } else if (rowO > 0) {
+                System.out.println("O wins");
+                break;
+            } else if (!space){
+                System.out.println("Draw");
+                break;
+            } else {
+                continue;
+                // System.out.println("Game not finished");
             }
         }
-
-        // Check game status
-//        for (int i = 0; i < 3; i++) {
-//            if (cellsArr[i][0] == cellsArr[i][1] && cellsArr[i][0] == cellsArr[i][2]) {
-//                if (cellsArr[i][0] == 'X') {
-//                    rowX++;
-//                } else if (cellsArr[i][0] == 'O') {
-//                    rowO++;
-//                }
-//            }
-//            if (cellsArr[0][i] == cellsArr[1][i] && cellsArr[0][i] == cellsArr[2][i]) {
-//                if (cellsArr[0][i] == 'X') {
-//                    rowX++;
-//                } else if (cellsArr[0][i] == 'O') {
-//                    rowO++;
-//                }
-//            }
-//        }
-//        if ((cellsArr[0][0] == cellsArr[1][1] && cellsArr[0][0] == cellsArr[2][2]) ||
-//                (cellsArr[0][2] == cellsArr[1][1] && cellsArr[0][2] == cellsArr[2][0])) {
-//            if (cellsArr[1][1] == 'X') {
-//                rowX++;
-//            } else if (cellsArr[1][1] == 'O') {
-//                rowO++;
-//            }
-//        }
-
-        // Output result
-//        if (x - o > 1 || o - x > 1 || (rowX > 0 && rowO > 0)) {
-//            System.out.println("Impossible");
-//        } else if (rowX > 0) {
-//            System.out.println("X wins");
-//        } else if (rowO > 0) {
-//            System.out.println("O wins");
-//        } else if (space) {
-//            System.out.println("Game not finished");
-//        } else {
-//            System.out.println("Draw");
-//        }
     }
 
     private static void printBoard(char[][] cells) {
